f283c441e17a9a65b1793c95282f233261af127e
AndreyZaklitskiy <glockman94@gmail.com>
2018-08-12 14:53:11 +0300

    pizza slider

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,4 +5,13 @@ $(document).ready(function(){
     prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
     nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>'
 });
+
+  $('.pizza-slider').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>'
+  });
+
 });
