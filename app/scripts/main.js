5fd35de9ad4993ab28bf05fb91a6eca003138694
AndreyZaklitskiy <glockman94@gmail.com>
2018-08-20 19:28:51 +0300

    button, without script

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,9 +9,15 @@ $(document).ready(function(){
   $('.pizza-slider').slick({
     infinite: true,
     slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToScroll: 3,
+    dots: true,
     prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
     nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>'
   });
 
+
+  //кнопка выбрать
+  $('.button').on('click', function() {
+    $(this).toggleClass('active');
+  });
 });
