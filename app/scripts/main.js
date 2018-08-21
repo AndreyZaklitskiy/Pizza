34a26ffc94849a9127a3b702b85235d341597154
AndreyZaklitskiy <glockman94@gmail.com>
2018-08-21 18:58:38 +0300

    chщÑoose pizza size

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,9 @@
-
 $(document).ready(function(){
+  //кнопка выбрать
+  $('.button').on('click', function() {
+    $(this).toggleClass('active');
+  });
+
   //Верхний слайдер (горячие предложения).
   $('.top-slider').slick({
     prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
@@ -15,9 +19,22 @@ $(document).ready(function(){
     nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>'
   });
 
+  // выбор размера пиццы
+  var ts = $('.pizza').find('.size-26');
+  var tt = $('.pizza').find('.size-32');
+  var price = $('.pizza').find('h3 > span');
+  var little = '26';
+  var big = '32';
 
-  //кнопка выбрать
-  $('.button').on('click', function() {
-    $(this).toggleClass('active');
+  ts.on('click', function(){
+    $(this).addClass('selected');
+    $(this).siblings().removeClass('selected');
   });
+  tt.on('click', function(){
+    $(this).siblings().removeClass('selected');
+    $(this).addClass('selected');
+  });
+  if(ts.hasClass('selected')) {
+
+  }
 });
