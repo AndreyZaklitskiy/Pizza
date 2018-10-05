60d1b1062d627f598b83160bad09ee77a4a22ba2
AndreyZaklitskiy <glockman94@gmail.com>
2018-10-06 00:38:14 +0300

    section.snack

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -10,11 +10,21 @@ $(document).ready(function(){
     prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
     nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>'
 });
-
+  //слайдер с пиццами
   $('.pizza-slider').slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 3,
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>'
+  });
+
+  //слайдер с закусками
+  $('.snack-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 2,
     dots: true,
     prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-chevron-left"></i></button>',
     nextArrow: '<button type="button" class="slick-next"><i class="fas fa-chevron-right"></i></button>'
